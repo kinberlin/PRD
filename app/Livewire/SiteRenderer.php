0d5bccd85942e8f8f3c32227a0995468770f13a0
Adiel Drystan <kinberlintchamba2003@gmail.com>
2024-05-03 15:32:25 +0100

    1st livewire commit

--- app/Livewire/SiteRenderer.php
+++ app/Livewire/SiteRenderer.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Enterprise;
+use App\Models\Site;
 use Livewire\Component;
 
 class SiteRenderer extends Component
 {
+    public $data = [];
+    public $ents = [];
+
+    public function mount()
+    {
+        $this->fill([
+            'data' => Site::all(),
+            'ents' => Enterprise::all()
+        ]);
+    }
     public function render()
     {
-        return view('livewire.site-renderer');
+        return view('livewire.admin.site-renderer');
     }
 }
