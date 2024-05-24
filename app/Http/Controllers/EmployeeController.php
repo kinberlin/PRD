6322bd02ae6ed05414e1aa15df6e7a4591f84711
Adiel Drystan <kinberlintchamba2003@gmail.com>
2024-05-24 12:07:22 +0100

    updated employee models and setting up pilote and rq authorisations

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
 use App\Models\Dysfunction;
 use App\Models\Enterprise;
 use App\Models\Site;
 use App\Models\Status;
+use App\Models\Users;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
@@ -27,5 +29,54 @@ class EmployeeController extends Controller
         $data = Dysfunction::all();
         $status = Status::all();
         return view('employees/listesignalement', compact('data', 'status'));
+    }
+        public function store(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $data = $request->input('data');
+            if (!is_array($data)) {
+                throw new Exception("Vous n'avez pas soumis de données a sauvegarder", 1);
+            }
+            foreach ($data as $row) {
+                DB::beginTransaction();
+                $employee = new Users();
+                $employee->enterprise = $row[1];
+                if ($row[2] == "null") {
+                    $employee->department = null;
+                } else {
+                    $employee->department = $row[2];
+                }
+                $employee->firstname = $row[3];
+                $employee->lastname = $row[4];
+                $employee->email = $row[5];
+                $employee->password = bcrypt($row[6]);
+                if (Enterprise::find($row[1]) == null) {
+                    throw new Exception("Nous ne parvenons pas a trouver l'entreprise dont l'ID est égal
+                a : " . $row[1] . ' dans notre systeme. Veuillez consulter la liste des entreprises et entrer un Identifiant valide.', 404);
+                }
+                if (Department::where('id', $row[2])->where('enterprise', $row[1]) == null) {
+                    throw new Exception("Nous ne parvenons pas a trouver le Département dont l'ID est égal
+                a : " . $row[2] . ' dans notre systeme. Veuillez consulter la liste des Départements dans l\'entreprise dont l\'ID est : ' . $row[1] . ' et entrer un Identifiant valide.', 404);
+                }
+                if (strtolower($row[7]) == "null") {
+                    $employee->phone = null;
+                } else {
+                    $employee->phone = $row[7];
+                }
+                if (strtolower($row[8]) == "null") {
+                    $employee->poste = null;
+                } else {
+                    $employee->poste = $row[8];
+                }
+                $employee->matricule = $row[10];
+                $employee->save();
+                DB::commit();
+            }
+            DB::commit();
+            return redirect()->back()->with('error', "Insertions terminées avec succes");
+        } catch (Throwable $th) {
+            return redirect()->back()->with('error', "Erreur : " . $th->getMessage());
+        }
     }
 }
---
+++ app/Models/AuthorisationRq.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int     $id
+ * @property int     $user
+ * @property int     $enterprise
+ * @property int     $created_at
+ * @property int     $deleted_at
+ * @property boolean $interim
+ */
+class AuthorisationRq extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'authorisation_rq';
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user', 'enterprise', 'created_at', 'deleted_at', 'interim'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'int', 'user' => 'int', 'enterprise' => 'int', 'created_at' => 'timestamp', 'deleted_at' => 'timestamp', 'interim' => 'boolean'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at', 'deleted_at'
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var boolean
+     */
+    public $timestamps = false;
+
+    // Scopes...
+
+    // Functions ...
+
+    // Relations ...
+}
