9c81df567a883b1e5c21e54ab3141f47bd3ebfe7
Adiel Drystan <kinberlintchamba2003@gmail.com>
2024-05-30 15:30:42 +0100

    initialising rq employee, rq and pilotes view

---
+++ resources/views/rq/employee.blade.php
@@ -0,0 +1,176 @@
+@extends('admin.theme.main')
+@section('title')
+    Gestion des Employés
+@endsection
+@section('manualstyle')
+@endsection
+@section('mainContent')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <!-- Users List Table -->
+        <div class="col-12">
+            <div class="card mb-4">
+                <h5 class="card-header">Employés</h5>
+                <div class="card-body">
+                    <div class="demo-inline-spacing">
+                        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEnd"
+                            aria-controls="offcanvasEnd">Ajouter un Employé</button>
+                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasEnd"
+                            aria-labelledby="offcanvasEndLabel" aria-modal="true" role="dialog">
+                            <div class="offcanvas-header">
+                                <h5 id="offcanvasEndLabel" class="offcanvas-title">Formulaire d'Ajout d'Employé</h5>
+                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body mx-0 flex-grow-0">
+                                <form class="add-new-user pt-0 fv-plugins-bootstrap5 fv-plugins-framework"
+                                    action="{!! route('admin.employee.onestore') !!}" method="POST">
+                                    <div class="mb-3 fv-plugins-icon-container">
+                                        @csrf
+                                        <label class="form-label" for="selents">Entreprise/Filiale</label>
+                                        <select id="selents" name="enterprise" class="form-select" required>
+                                            @foreach ($ents as $e)
+                                                <option value="{{ $e->id }}" data-extra-info="{{ $e->id }}">
+                                                    {{ $e->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="mb-3 fv-plugins-icon-container">
+                                        <label class="form-label" for="seldep">Département</label>
+                                        <select id="seldep" name="department" class="form-select" required>
+                                            @foreach ($deps as $d)
+                                                <option value="{{ $d->id }}"
+                                                    data-extra-info="{{ $d->enterprise }}">
+                                                    {{ $d->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="mb-3 fv-plugins-icon-container">
+                                        <label class="form-label" for="firstname">Nom</label>
+                                        <input type="text" class="form-control" id="firstname" placeholder="Nom de l'employé..."
+                                            name="firstname" aria-label="Cadyst">
+                                        <div
+                                            class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback">
+                                        </div>
+                                    </div>
+                                    <div class="mb-3 fv-plugins-icon-container">
+                                        <label class="form-label" for="lastname">Prenom</label>
+                                        <input type="text" class="form-control" id="lastname" placeholder="Prenom de l'employé..."
+                                            name="lastname" aria-label="Cadyst">
+                                    </div>
+                                    <div class="mb-3 fv-plugins-icon-container">
+                                        <label class="form-label" for="idmat">Matricule</label>
+                                        <input type="text" class="form-control" id="idmat" placeholder="Matricule de l'employé..."
+                                            name="matricule" aria-label="Cadyst">
+                                    </div>
+                                    <div class="mb-3 fv-plugins-icon-container">
+                                        <label class="form-label" for="idemail">Email</label>
+                                        <input type="email" class="form-control" id="idemail" placeholder="Email de l'employé..."
+                                            name="email" aria-label="Cadyst">
+                                    </div>
+                                    <div class="mb-3 fv-plugins-icon-container">
+                                        <label class="form-label" for="idphone">Telephone</label>
+                                        <input type="tel" class="form-control" id="idphone" placeholder="Tel. de l'employé..."
+                                            name="phone" aria-label="Cadyst">
+                                        <div
+                                            class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback">
+                                        </div>
+                                    </div>
+                                    <div class="mb-3 fv-plugins-icon-container">
+                                        <label class="form-label" for="idposte">Poste Occupé</label>
+                                        <input type="text" class="form-control" id="idposte" placeholder="Poste de l'employé..."
+                                            name="poste" aria-label="Cadyst">
+                                    </div>
+                                    <button type="submit"
+                                        class="btn btn-primary me-sm-3 me-1 data-submit">Ajouter</button>
+                                    <button type="reset" class="btn btn-label-secondary"
+                                        data-bs-dismiss="offcanvas">Annuler</button>
+                                    <input type="hidden">
+                                </form>
+                            </div>
+                        </div>
+                        <button type="button" class="btn btn-info" id="importBtn">Importer depuis un fichier</button>
+                        <button type="button" class="btn btn-success" id="exportXlsxBtn">Exporter le tableau vers
+                            Excel</button>
+                        <a href="{!! url('assets/extras/cadyst_liste_employee_modele.xlsx') !!}" class="btn btn-secondary">Télécharger le Modele</a>
+                        <input type="file" style="visibility: hidden" id="excelFileInput">
+
+                        <form action="{!! route('admin.employee.store') !!}" method="POST">
+                            @csrf
+                            <table id="dataTable" class="display" style="width:100%">
+                            </table>
+                            <button id="checkAllBtn" class="secondary-btn">Vérifier</button>
+                            <button id="submitBtn" type="submit">Soumettre</button>
+                        </form>
+                    </div>
+                </div>
+                <hr class="m-0">
+            </div>
+        </div>
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title">Liste des Employés sur PRD</h5>
+                <div class=" align-items-start justify-content-between">
+                    <table id="datatables-orders"
+                        class="table table-striped datatables-basic table border-top dataTable no-footer dtr-column">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Entreprise</th>
+                                <th>Département</th>
+                                <th>Noms</th>
+                                <th>Email</th>
+                                <th>Téléphone</th>
+                                <th>Matricule</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $d)
+                                <tr>
+                                    <td>{{ $d->id }}</td>
+                                    <td>{{ $ents->where('id', $d->enterprise)->first()->name }}</td>
+                                    @if ($deps->where('id', $d->department)->first() != null)
+                                        <td>{{ $deps->where('id', $d->department)->first()->name }}</td>
+                                    @else
+                                        <td>Aucun département renseigner.</td>
+                                    @endif
+                                    <td>{{ $d->firstname }} {{ $d->lastname }}</td>
+                                    <td>{{ $d->email }}</td>
+                                    <td>{{ $d->phone }}</td>
+                                    <td>{{ $d->matricule }}</td>
+                                    <td><button type="button" class="btn btn-info btn-sm" data-bs-toggle="modal"
+                                            data-bs-target="#majemployeerr{{ $d->id }}">
+                                            M.A.J
+                                        </button></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+@section('scriptContent')
+    <script src="{!! url('assets/vendor/libs/select2/select2.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/%40form-validation/popular.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/%40form-validation/bootstrap5.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/%40form-validation/auto-focus.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/cleavejs/cleave.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/cleavejs/cleave-phone.js') !!}"></script>
+
+    <script src="{!! url('assets/js/js/accessory.js') !!}"></script>
+    <script>
+        $(document).ready(function() {
+            $('#selents').change(function() {
+                var selectedOption = $(this).find(':selected');
+                var selectedValue = selectedOption.attr('data-extra-info');
+                $('#seldep option').hide();
+                $('#seldep option[data-extra-info="' + selectedValue + '"]').show();
+                $('#seldep').val($('#selsite option:visible:first').val());
+            });
+        });
+    </script>
+@endsection
---
+++ resources/views/rq/pltemployee.blade.php
@@ -0,0 +1,119 @@
+@extends('admin.theme.main')
+@section('title')
+    Gestion des Pilotes de Processus
+@endsection
+@section('manualstyle')
+    @livewireStyles
+@endsection
+@section('mainContent')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <!-- Users List Table -->
+        <div class="col-12">
+            <div class="card mb-4">
+                <h5 class="card-header">Pilotes</h5>
+                <div class="card-body">
+                    <div class="demo-inline-spacing">
+                        <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasEnd"
+                            aria-controls="offcanvasEnd">Ajouter un Pilote</button>
+                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasEnd"
+                            aria-labelledby="offcanvasEndLabel" aria-modal="true" role="dialog">
+                            <div class="offcanvas-header">
+                                <h5 id="offcanvasEndLabel" class="offcanvas-title">Formulaire d'Ajout de Pilote</h5>
+                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                    aria-label="Close"></button>
+                            </div>
+                            @livewire('addplt-employee-form')
+                        </div>
+                        <button type="button" class="btn btn-success" id="exportXlsxBtn">Exporter le tableau vers
+                            Excel</button>
+                    </div>
+                </div>
+                <hr class="m-0">
+            </div>
+        </div>
+        <div class="card">
+
+            <div class="card-body">
+                <h5 class="card-title">Liste des Pilotes sur PRD</h5>
+                <div class=" align-items-start justify-content-between">
+                    <table id="datatables-orders"
+                        class="table table-striped datatables-basic table border-top dataTable no-footer dtr-column">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Infos sur <br> le Pilote</th>
+                                <th>Processus d'<br>Action & Role</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $d)
+                                <tr>
+                                    <td>{{ $d->id }}</td>
+                                    <td>
+                                        Entreprise :
+                                        <b>{{ $ents->where('id', $users->where('id', $d->user)->first()->enterprise)->first()->name }}</b>
+                                        <br>Noms :
+                                        <b>{{ $users->where('id', $d->user)->first()->firstname .' '. $users->where('id', $d->user)->first()->lastname }}</b>
+                                        <br>Email : <b>{{ $users->where('id', $d->user)->first()->email }}</b>
+                                        <br>Tel. <b>{{ $users->where('id', $d->user)->first()->phone }} | {{ $users->where('id', $d->user)->first()->matricule }}</b>
+                                    </td>
+                                    <td>Processus : {{ $processes->where('id', $d->process)->first()->name . ' ('.$processes->where('id', $d->process)->first()->surfix.')' }}
+                                        <br> Rôle : Pilote @if ($d->interim == 1)
+                                            en Intérim
+                                        @else
+                                            Principale
+                                        @endif
+                                    </td>
+                                    <td><button type="button" class="btn btn-danger fs-5 fw-bold" data-bs-toggle="modal"
+                                            data-bs-target="#delauthRQ{{ $d->id }}">
+                                            Retirer l'authorisation
+                                        </button>
+                                        <div class="modal modal-top fade" id="delauthRQ{{ $d->id }}"
+                                            tabindex="-1">
+                                            <div class="modal-dialog">
+                                                <form class="modal-content">
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title" id="modalTopTitle">Confirmation de
+                                                            supression!</h5>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div class="card-body">
+                                                            <p class="card-text">
+                                                                En continuant, vous allez supprimer l'authorisation RQ possèdé par cet utilisateur. Voulez vous Continuer ?
+                                                                <b>Noter que cette action est irréversible!</b>
+                                                            </p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-label-secondary"
+                                                            data-bs-dismiss="modal">Fermer</button>
+                                                        <a href="{{ route('admin.authplt.destroy', ['id'=>$d->id]) }}"
+                                                            class="btn btn-danger">Continuer</a>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
+@section('scriptContent')
+@livewireScripts
+    <script src="{!! url('assets/vendor/libs/select2/select2.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/%40form-validation/popular.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/%40form-validation/bootstrap5.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/%40form-validation/auto-focus.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/cleavejs/cleave.js') !!}"></script>
+    <script src="{!! url('assets/vendor/libs/cleavejs/cleave-phone.js') !!}"></script>
+    <script src="{!! url('assets/js/js/form-layouts.js') !!}"></script>
+    <script src="{!! url('assets/js/js/accessory.js') !!}"></script>
+@endsection
